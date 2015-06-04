39ab19fa46a797489c3a8a9d86b6e9850dbe0d4c
Shiv Sakhuja <shivsak.s@gmail.com>
2015-06-04 23:53:09 +0530

    Initial Commit

---
+++ CustomAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAlertView
+//
+//  Created by Shiv Sakhuja on 6/4/15.
+//  Copyright (c) 2015 Shiv Sakhuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertView
+//
+//  Created by Shiv Sakhuja on 6/4/15.
+//  Copyright (c) 2015 Shiv Sakhuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
